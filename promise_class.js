0fa0df2a25d7eb4bf71c94aea8683c6efdcac4ca
KylieHan0306 <163808985+KylieHan0306@users.noreply.github.com>
2024-06-18 21:42:38 +1000

    Add files via upload

---
+++ promise_class.js
@@ -0,0 +1,177 @@
+class Promise{
+
+    constructor(executor) {
+        // Add attribute
+        this.PromiseState = 'pending';
+        this.PromiseResult = null;
+        this.callbacks = [];
+
+        const self = this; 
+
+        function res(data) {
+            /**
+             * Modify the state and result
+             * 'this' refers to the window, so we cannot directly use 'this'
+             * Check whether the state has been changed, the state of a promise can only be changed once
+            */
+            if(self.PromiseState !== 'pending') return;
+            self.PromiseState = 'fulfilled';
+            self.PromiseResult = data;
+            /**
+             * Call the success callback function, timing of calling the callback function when asynchronous
+             * Execute the callback multiple times
+            */
+            setTimeout(()=>{
+                self.callbacks.forEach(item => {
+                    item.onResolved(data);
+                })
+            });
+        }
+
+        function reject(data) {
+            if(self.PromiseState !== 'pending') return;
+            self.PromiseState = 'rejected';
+            self.PromiseResult = data;
+            setTimeout(()=>{
+                self.callbacks.forEach(item => {
+                    item.onResolved(data);
+                })
+            });
+            /* Execute the callback only once
+            if(self.callback.onRejected) {
+                self.callback.onRejected(data);
+            }*/
+        }
+
+        try {
+            // Synchronously output executor function
+            executor(res, reject);
+        } catch(e) {
+            reject(e);
+        }
+    }
+
+    then(onResolved, onRejected) {
+        let self = this; 
+        if(typeof onRejected !== 'function') {
+            onRejected = reason => {
+                throw reason;
+            }
+        }
+    
+        if(typeof onResolved !== 'function') {
+            onResolved = value => value;
+        }
+    
+        return new Promise((resolve, reject) => {
+    
+            function callback(type) {
+                /**
+                 * If a non-promise result is returned, result will be a successful promise
+                 * If a promise result is returned, result will be the returned promise
+                */
+                try {
+                    let result = type(self.PromiseResult);
+                    if (result instanceof Promise) {
+                        result.then(v => {
+                            resolve(v);
+                        }, r=>{
+                            reject(r);
+                        });
+                    } else {
+                        resolve(result);
+                    }
+                } catch(e) {
+                    reject(e);
+                }
+            }
+    
+            // Call the fulfilled & rejected callback functions synchronously
+            if(this.PromiseState === 'fulfilled') {
+                //setTimeOut makes then execute asynchronously
+                setTimeout(()=>{
+                    callback(onResolved);
+                });
+            }
+            if(this.PromiseState === 'rejected') {
+                setTimeout(()=>{
+                    callback(onRejected);
+                });
+            }
+            //Check if pending, when asynchronous save callback functions
+            if(this.PromiseState === 'pending') {
+                this.callbacks.push({
+                    onResolved: function() {
+                        callback(onResolved);
+                    },
+                    onRejected: function() {
+                        callback(onRejected);
+                    }
+                });
+            }
+        })
+    
+    }
+
+    catch(onRejected) {
+        return this.then(undefined, onRejected);
+    }
+
+    static resolve(value) {
+        return new Promise((res, rej) => {
+            if (value instanceof Promise) {
+                value.then(v=> {
+                    res(v);
+                }, r=>{
+                    rej(r);
+                })
+            } else {
+                res(value);
+            }
+        });
+    }
+
+    static reject(reason) {
+        return new Promise((res, rej) => {
+            rej(reason);
+        });   
+    }
+
+    static all(promises) {
+        return new Promise((res, rej) => {
+            let count = 0; 
+            let arr = [];
+    
+            for(let i = 0; i < promises.length; i++) {
+    
+                promises[i].then(v => {
+                    count++;
+                    /**
+                     * There is an asynchronous situation,
+                     * If you don't use 'i' to change the state first, the Promise will be placed into the array first,
+                     * leading to an incorrect order
+                    */
+                    arr[i] = v;
+                    if(count === promises.length) {
+                        res(arr);
+                    }
+                }, r => {
+                    rej(r);
+                })
+            }
+        });
+    }
+
+    static race(promises) {
+        return new Promise((res, rej) => {
+
+            for(let i = 0; i < promises.length; i++) {
+                promises[i].then(v => {
+                    res(v);
+                }, r => {
+                    rej(r);
+                })
+            }
+        });
+    }
+}
